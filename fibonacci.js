3384031f04ccbea46545708bbd16132999f0c43d
singh0471 <142919646+singh0471@users.noreply.github.com>
2024-09-19 23:56:04 +0530

    fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,16 @@
+function sumOfFibonacci() {
+    let a = 0, b = 1;
+    let sum = a + b;
+
+    while (true) {
+        let next = a + b;
+        if (next > 5000) break;
+        sum += next;
+        a = b;
+        b = next;
+    }
+
+    return sum;
+}
+
+console.log(`Sum of Fibonacci series up to 5000 is: ${sumOfFibonacci()}`);
